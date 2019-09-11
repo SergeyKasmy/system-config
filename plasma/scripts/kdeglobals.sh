8597927bfe899ba89f9ce81afd3a6a58f18beb44
Sergey Kasmy <SleeplessSloth79@gmail.com>
2019-09-12 02:45:29 +0300

    plasma: remove everything related to Plasma

--- plasma/scripts/kdeglobals.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# open files with double-click
-kwriteconfig5 --file 'kdeglobals' --group 'KDE' --key 'SingleClick' --type bool false
--- plasma/scripts/klipper.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# do not copy selection to the clipboard
-kwriteconfig5 --file 'klipperrc' --group 'General' --key 'IgnoreSelection' --type bool true
