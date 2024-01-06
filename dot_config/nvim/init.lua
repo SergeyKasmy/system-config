f173f103b4d6259026ba5ca70013c7ddf4f2ec93
Sergey Kasmy <SleeplessSloth79@gmail.com>
2024-01-06 20:55:04 +0100

    nvim: cleanup & update plugins + enable neovim 0.10 inlay hints

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -17,53 +17,51 @@ if not vim.loop.fs_stat(lazypath) then
     lazypath,
   })
 end
-print(lazypath)
 vim.opt.rtp:prepend(lazypath)
 
 --- plugins
 require('lazy').setup('plugins')
 
-local map = require('map')
+
+local map = vim.keymap.set
 map('i', 'jk', '<ESC>')
 map('t', '<C-j><C-k>', '<C-\\><C-n>')
 map('n', '<Leader>s', ':set spell!<enter>')
 
 --- options
-vim.opt.shortmess = vim.opt.shortmess + 'c' 	-- avoid showing short messages -- TODO: what short messages are you even talking about???
-vim.opt.completeopt = {'menu', 'menuone', 'noselect'} -- TODO: mb add noinsert
-vim.opt.signcolumn = 'yes'
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.mouse = 'a'
-vim.opt.splitright = true
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.updatetime = 500
-vim.opt.undofile = true
--- vim.opt.list = true
--- vim.opt.listchars = 'tab:| '
-vim.opt.foldlevelstart = 99
-vim.opt.cursorline = true
+local opt = vim.opt
+opt.shortmess = vim.opt.shortmess + 'c' 	-- avoid showing short messages -- TODO: what short messages are you even talking about???
+opt.completeopt = {'menu', 'menuone', 'noselect'} -- TODO: mb add noinsert
+opt.signcolumn = 'yes'
+opt.number = true
+opt.relativenumber = true
+opt.mouse = 'a'
+opt.splitright = true
+opt.ignorecase = true
+opt.smartcase = true
+opt.updatetime = 500
+opt.undofile = true
+-- opt.list = true
+-- opt.listchars = 'tab:| '
+opt.foldlevelstart = 99
+opt.cursorline = true
 
 -- tabs
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = false
-vim.opt.smarttab = false
-vim.opt.smartindent = true
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = false
+opt.smarttab = false
+opt.smartindent = true
 
 -- code
-vim.cmd 'filetype plugin indent on'
 vim.g.rust_recommended_style = 0	-- disable tabs in rust
 
 --- ui
 vim.wo.foldmethod = 'expr'
 
--- TODO: replace with vim.api.nvim_create_autocmd and user_command()
-
---- other
--- cmd 'command! Reload :source $MYVIMRC'
-vim.cmd 'command! Cfg :edit $MYVIMRC'
-
 -- highlight on yank
-vim.cmd 'autocmd TextYankPost * lua vim.highlight.on_yank { timeout = 300 }'
+vim.api.nvim_create_autocmd('TextYankPost', {
+	callback = function()
+		vim.highlight.on_yank({ timeout = 300 })
+	end
+})
--- dot_config/nvim/lua/map.lua
+++
@@ -1,6 +0,0 @@
-return function(mode, key, command, opts)
-	local options = {noremap = true}
-	if opts then options = vim.tbl_extend('force', options, opts) end
-	vim.api.nvim_set_keymap(mode, key, command, options)
-end
-
