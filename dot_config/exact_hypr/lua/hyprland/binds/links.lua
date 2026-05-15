20c2624a5599bf3035bd85f08070caf719c22ee1
Sergey Kasmy <sergey.kasmy@gmail.com>
2026-05-15 04:14:26 +0200

    hyprland: extract links into a separate file

---
+++ dot_config/exact_hypr/lua/hyprland/binds/links.lua
@@ -0,0 +1,9 @@
+return {
+  anime = "https://anilist.co/home",
+  search = "https://bing.com/",
+  email = "https://mail.google.com/",
+  photos = "https://photos.google.com/",
+  maps = "https://maps.google.com/",
+  steam = "https://steamcommunity.com/my/profile",
+  youtube = "https://youtube.com",
+}
