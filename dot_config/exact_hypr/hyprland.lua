987ba4d74bf746762c52687cbcea0806520e4c29
Sergey Kasmy <sergey.kasmy@gmail.com>
2026-05-20 17:08:29 +0200

    hyprland: move config proxy into a separate file

--- dot_config/exact_hypr/hyprland.lua
+++ dot_config/exact_hypr/hyprland.lua
@@ -1,5 +1,4 @@
 local log = require("lua.log")
-
 log.config.min_log_level = "INFO"
 
 log.spanned_require("monitors", "lua.hyprland.monitors")
@@ -15,31 +14,5 @@ log.spanned("autorun", function()
   hl.on("hyprland.start", require("lua.hyprland.autostart"))
 end)
 
-local function make_config_proxy(path, depth)
-  depth = depth or 0
-  local proxy = {}
-  setmetatable(proxy, {
-    __newindex = function(_, key, value)
-      local result = { [key] = value }
-      for i = depth, 1, -1 do
-        result = { [path[i]] = result }
-      end
-      hl.config(result)
-    end,
-    __index = function(_, key)
-      local child_depth = depth + 1
-      local child_path = {}
-      for i = 1, depth do
-        child_path[i] = path[i]
-      end
-      child_path[child_depth] = key
-      return make_config_proxy(child_path, child_depth)
-    end,
-    __call = function(_)
-      return hl.get_config(table.concat(path, ".", 1, depth))
-    end,
-  })
-  return proxy
-end
 
-Config = make_config_proxy({}, 0)
+Config = require("lua.hyprland.config_proxy")()
