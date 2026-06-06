07857c3082d7a04c421361a1e401a9a8e4460cba
Sergey Kasmy <sergey.kasmy@gmail.com>
2026-06-06 19:54:38 +0200

    hyprland: migrate Iter adapters to tbl utils, add .new & .clone in the process

--- dot_config/exact_hypr/lua/lib/iter/filter.lua
+++ dot_config/exact_hypr/lua/lib/iter/filter.lua
@@ -1,11 +1,23 @@
 local Iter = require("lua.lib.iter").Iter
+local tbl = require("lua.lib.table")
 
 ---@generic T
 ---@class FilterIter<T> : Iter<T>
 ---@field source Iter<T>
 ---@field pred fun(v: T): boolean
-local FilterIter = setmetatable({}, { __index = Iter }) --[[@as FilterIter]]
-FilterIter.__index = FilterIter
+local FilterIter = tbl.type(Iter)
+
+---@generic T
+---@param source Iter<T>
+---@param pred fun(v: T): boolean
+---@return FilterIter<T>
+function FilterIter.new(source, pred)
+  return tbl.instance_of(FilterIter, { source = source, pred = pred })
+end
+
+function FilterIter:clone()
+  return tbl.instance_of(FilterIter, { source = self.source:clone(), pred = self.pred })
+end
 
 function FilterIter:next()
   while true do
--- dot_config/exact_hypr/lua/lib/iter/map.lua
+++ dot_config/exact_hypr/lua/lib/iter/map.lua
@@ -1,11 +1,23 @@
 local Iter = require("lua.lib.iter").Iter
+local tbl = require("lua.lib.table")
 
 ---@generic T, U
 ---@class MapIter<T, U> : Iter<U>
 ---@field source Iter<T>
 ---@field f fun(v: T): U
-local MapIter = setmetatable({}, { __index = Iter }) --[[@as MapIter]]
-MapIter.__index = MapIter
+local MapIter = tbl.type(Iter)
+
+---@generic T, U
+---@param source Iter<T>
+---@param f fun(v: T): U
+---@return MapIter<T, U>
+function MapIter.new(source, f)
+  return tbl.instance_of(MapIter, { source = source, f = f })
+end
+
+function MapIter:clone()
+  return tbl.instance_of(MapIter, { source = self.source:clone(), f = self.f })
+end
 
 function MapIter:next()
   local v = self.source:next()
